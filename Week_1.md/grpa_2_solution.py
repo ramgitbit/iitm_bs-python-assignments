dd9dbb1ee3e9d096aa53a6b47717ec6ceded4ade
ramgitbit <rambhawan111777@gmail.com>
2026-03-29 15:50:53 +0530

    Add GRPA 2 solution script for Week 1 assignment

---
+++ Week_1.md/grpa_2_solution.py
@@ -0,0 +1,27 @@
+# Sample inputs (# note: The values given in the prefix code(grey) will be changed by the autograder according to the testcase while running them.
+a = 5
+price1, discount1 = 50, 4 # for offer1
+price2, discount2 = 60, 8 # for offer2
+
+# Assume discount is given in percentages
+# <eoi>
+output1 = a >= 5  # bool: True if a greater than or equal to 5
+
+output2 = a %5 ==0 # bool: True if a is divisible by 5
+
+output3 = a%2 !=0 and a <10 # bool: True if a is odd number less than 10
+
+output4 = a %2 != 0 and a>-10 and a <10 # bool: True if a is an odd number within the range -10 and 10
+
+output5 = len(str(a)) % 2 ==0 and len(str(a)) <= 10# bool: True if a has even number of digits but not more than 10 digits
+
+is_offer1_cheaper = (price1*(1-discount1/100)) < (price2*(1-discount2/100)) # bool: True if the offer1 is strictly 
+# Discount(4% or 8%) ko decimal me convert karke (discount/100), usse 1 me se minus karte hain(1 ka matlab 100% hota hai) to remaining percentage milta hai
+# Fir us remaining value ko price se multiply karke final discounted price nikalte hain
+print(output1)
+print(output2)
+print(output3)
+print(output4)
+print(output5)
+print(is_offer1_cheaper)
+
